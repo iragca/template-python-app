dabc915be985f4889c7d4ab815f8cba13aad9ecd
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-07-16 20:27:09 +0800

    feat: add ensure_path utility function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
 from .check_env_variable import check_env_variable
 from .greetings import greetings
+from .ensure_path import ensure_path
 
 
-__all__ = ["check_env_variable", "greetings"]
+__all__ = ["check_env_variable", "greetings", "ensure_path"]
---
+++ src/utils/ensure_path.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+def ensure_path(path: Path) -> None:
+    """
+    Ensure that the given path is a Path object.
+    If the path does not exist, it will create the directory.
+
+    Args:
+        path (str): The path to be converted.
+
+    Returns:
+        Path: A Path object representing the given path.
+    """
+    assert isinstance(path, Path), f"Expected Path type, got {type(path)}"
+
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+        print(f"Directory not found. Created it instead: {path}")
+
+    return path
