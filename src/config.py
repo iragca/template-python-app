3439a3718ff759d9c13cbaa2dd9cf643c1c206b2
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-07-17 08:40:30 +0800

    refactor: encapsulate directory paths in DirectoryPaths enum and update logger references

--- src/config.py
+++ src/config.py
@@ -5,19 +5,26 @@ from enum import Enum
 from dotenv import load_dotenv
 from loguru import logger
 
-from src.utils import check_env_variable
+from src.utils import check_env_variable, ensure_path
 
-PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent
-ENV_FILE = PROJECT_ROOT / ".env"
-DATA_DIR = PROJECT_ROOT / "data"
 
-if not ENV_FILE.exists():
+class DirectoryPaths(Enum):
+    PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent
+    ENV_FILE = ensure_path(PROJECT_ROOT / ".env")
+    DATA_DIR = ensure_path(PROJECT_ROOT / "data")
+
+
+_PROJECT_ROOT = DirectoryPaths.PROJECT_ROOT.value
+_DATA_DIR = DirectoryPaths.DATA_DIR.value
+_ENV_FILE = DirectoryPaths.ENV_FILE.value
+
+if not _ENV_FILE.exists():
     logger.warning(
-        f".env file not found at {ENV_FILE}. Please create one with the required environment variables."
+        f".env file not found at {_ENV_FILE}. Please create one with the required environment variables."
     )
 else:
-    load_dotenv(dotenv_path=ENV_FILE)
-    logger.info(f"Loaded environment variables from {ENV_FILE}")
+    load_dotenv(dotenv_path=_ENV_FILE)
+    logger.info(f"Loaded environment variables from {_ENV_FILE}")
 
 required_env_vars = []
 non_essential_env_vars = []
@@ -34,10 +41,10 @@ for var in non_essential_env_vars:
 class Environment(Enum):
     """Settings class to hold environment variables."""
 
-    # Add more environment variables as needed
-    # EXAMPLE = os.getenv("example")
+    # Example:
+    # DB_URL = os.getenv("DB_URL")
 
 
 # Log key paths
-logger.info(f"PROJECT_ROOT: {PROJECT_ROOT}")
-logger.info(f"DATA_DIR: {DATA_DIR}")
+logger.info(f"PROJECT_ROOT: {_PROJECT_ROOT}")
+logger.info(f"DATA_DIR: {_DATA_DIR}")
